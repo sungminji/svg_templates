3d9434fbcd461a71810d13612873de1df9f2bdaa
minji Sung <minji.sung.931009@gmail.com>
2023-03-28 16:12:34 +0900

    [minji]init set

---
+++ svg_templates/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Hello world!!!</h1>
+    </div>
+  );
+};
+
+export default App;
